dadf31dbdf4ae4a49ef87ba5943c3d62a3cd0d6b
admin <bremen@fastwebnet.it>
2021-03-30 18:08:11 +0200

    next-conf update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   /* config options here */
-  basePath: isProd ? '/build' : '',
-  assetPrefix: isProd ? '/build' : '/',
+  basePath: isProd ? '/cb_web' : '',
+  assetPrefix: isProd ? '/cb_web' : '/',
   env: {
-    BASE_URL: isProd ? '/build' : '',
-    BACKEND_URL: isProd ? 'https://facility-hub.cineca.it' : 'https://facility-hub.cineca.it',
-    HODGKIN_HUXLEY_BASE_URL: 'https://hbp-bsp-hhnb.cineca.it:17895/hh-neuron-builder',
+    BASE_URL: isProd ? '/cb_web' : '',
+    BACKEND_URL: isProd ? 'http://192.167.82.167:17895/' : 'http://192.167.82.167:17895/',
+    HODGKIN_HUXLEY_BASE_URL: 'https://192.167.82.167:17895/hh-neuron-builder/hhf-comm',
     SUGGESTION_MIN_LENGTH: 1
   },
 }
