4df5b7bf3683010ef7a968b28e75b1cf08a3e2da
admin <bremen@fastwebnet.it>
2021-04-21 10:53:02 +0200

    added the modified files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 const isProd = process.env.NODE_ENV === 'production';
+console.log('@@@@isProd', isProd);
 module.exports = {
   /* config options here */
   basePath: isProd ? '/cb_web' : '',
   assetPrefix: isProd ? '/cb_web' : '/',
   env: {
     BASE_URL: isProd ? '/cb_web' : '',
-    BACKEND_URL: isProd ? 'http://192.167.82.167:17895/' : 'http://192.167.82.167:17895/',
-    HODGKIN_HUXLEY_BASE_URL: 'https://192.167.82.167:17895/hh-neuron-builder/hhf-comm',
+    BACKEND_URL: isProd ? 'http://90.147.109.105:17895' : 'http://90.147.109.105:17895',
+    HODGKIN_HUXLEY_BASE_URL: 'https://90.147.109.105:17895/hh-neuron-builder/hhf-comm',
     SUGGESTION_MIN_LENGTH: 1
   },
 }
